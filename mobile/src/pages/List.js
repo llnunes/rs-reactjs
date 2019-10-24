d8843f6ed887e520a7dda2ac55020b9cf6eafbd2
lucas.lopes <lucas.nunes@ana.gov.br>
2019-10-24 18:56:06 -0300

    Ajustes no mobile

--- mobile/src/pages/List.js
+++ mobile/src/pages/List.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, Text, StyleSheet, Image, AsyncStorage } from 'react-native';
 
-export default function Login() {
+import logo from '../assets/logo.png';
+
+export default function List() {
+  const [techs, setTechs] = useState([]);
+
+  useEffect (() => {
+    AsyncStorage.getItem('techs').then(storagedTechs => {
+      const techsArray = storagedTechs.split(',').map(tech => tech.trim()); 
+      setTechs(techsArray);
+    })
+  }, []);
   return (
-    <View />
+    <SafeAreaView style={styles.container}>
+      <Image style={styles.logo} source={logo} />
+      
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }, 
+  logo : {
+    height: 32,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 30
+  }
+});
