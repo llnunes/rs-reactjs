66ceec58f0da2559fd10929b2a02f93d562c5c11
lucas.lopes <lucas.nunes@ana.gov.br>
2019-10-25 18:26:56 -0300

    ajustes

--- mobile/src/pages/Book.js
+++ mobile/src/pages/Book.js
@@ -52,37 +52,41 @@ const styles = StyleSheet.create({
   container: {
     margin: 30,
   },
+
+  label: {
+    fontWeight: 'bold',
+    color: '#444',
+    marginBottom: 8,
+    marginTop: 30,
+  },
+
   input: {
     borderWidth: 1,
     borderColor: '#ddd',
-    paddingHorizontal: 20, 
+    paddingHorizontal: 20,
     fontSize: 16,
     color: '#444',
     height: 44,
     marginBottom: 20,
     borderRadius: 2
   },
-  label: {
-    fontWeight: 'bold',
-    color: '#444',
-    marginBottom: 8,
-    marginTop: 30,
-  }, 
+
   button: {
-    height: 42, 
+    height: 42,
     backgroundColor: '#f05a5b',
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 2,
   },
-  buttonCancel: {
+
+  cancelButton: {
     backgroundColor: '#ccc',
-    marginTop: 10,
+    marginTop: 10
   },
+
   buttonText: {
     color: '#FFF',
     fontWeight: 'bold',
-    fontSize: 15,
-  } 
-
+    fontSize: 16,
+  },
 });
