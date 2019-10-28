ebbe614cea34c9e8f34c349259e6406d7c963948
lucas <lucas.nunes@basis.com.br>
2019-10-28 17:44:52 -0300

    Finalizado implementação

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -8,13 +8,18 @@ module.exports = {
         const { date } = req.body;
 
         const booking = await Booking.create({
-            user: user_id,
-            
+            user: user_id,            
             spot: spot_id,
             date,
         });
 
         await booking.populate('spot').populate('user').execPopulate();
+
+        const ownerSocket = req.connectedUsers[booking.spot.user];
+
+        if(ownerSocket) {
+            req.io.to(ownerSocket).emit('booking_request', booking);
+        }
 
         return res.json(booking);
     }
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 
+const socketio = require('socket.io');
+const http = require('http');
+
 const routes = require('./routes');
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
 
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect ('mongodb://localhost/mymoney', {
@@ -13,7 +20,17 @@ module.exports = mongoose.connect ('mongodb://localhost/mymoney', {
     useUnifiedTopology: true,
 });
 
+io.on('connection', socket => {
+    const { user_id } = socket.handshake.query;
 
+    connectedUsers[user_id] = socket.id;
+})
+
+app.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+    return next();
+})
 // GET, POST, PUT, DELETE
 //app.get('/users ...
 //req.query.? -> Acessar os query params (para filtros)
@@ -31,4 +48,4 @@ app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
-app.listen(3333);
+server.listen(3333);
